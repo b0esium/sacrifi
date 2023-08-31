63b5d458074f99f760bd46c05436dcd968921a7f
b0esium <b0esium@protonmail.com>
2023-08-31 18:10:12 +0200

    Add sacrifice UI

---
+++ app/components/NFTDisplay.js
@@ -0,0 +1,60 @@
+export default function NFTDisplay() {
+    return (
+        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+            <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+                <h2 className={`mb-3 text-2xl font-semibold`}>
+                    NFT 1{" "}
+                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                        -&gt;
+                    </span>
+                </h2>
+                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>NFT information</p>
+            </div>
+
+            <a
+                href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <h2 className={`mb-3 text-2xl font-semibold`}>
+                    NFT 2{" "}
+                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                        -&gt;
+                    </span>
+                </h2>
+                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>NFT information</p>
+            </a>
+
+            <a
+                href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <h2 className={`mb-3 text-2xl font-semibold`}>
+                    NFT 3{" "}
+                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                        -&gt;
+                    </span>
+                </h2>
+                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>NFT information</p>
+            </a>
+
+            <a
+                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <h2 className={`mb-3 text-2xl font-semibold`}>
+                    NFT 4{" "}
+                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+                        -&gt;
+                    </span>
+                </h2>
+                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>NFT information</p>
+            </a>
+        </div>
+    )
+}
