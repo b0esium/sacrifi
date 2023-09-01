23c8b77cadc3962c264ea8bada611088fc5307f5
b0esium <b0esium@protonmail.com>
2023-09-01 17:20:30 +0200

    Display NFT images

--- next.config.js
+++ next.config.js
@@ -2,10 +2,21 @@
 const nextConfig = {
     reactStrictMode: true,
     webpack: (config) => {
-        config.resolve.fallback = { fs: false, net: false, tls: false };
-        config.externals.push('pino-pretty', 'lokijs', 'encoding');
-        return config;
-    }
+        config.resolve.fallback = { fs: false, net: false, tls: false }
+        config.externals.push("pino-pretty", "lokijs", "encoding")
+        return config
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "nft-cdn.alchemy.com",
+                port: "",
+                pathname: "/eth-mainnet/**",
+            },
+        ],
+        dangerouslyAllowSVG: true,
+    },
 }
 
 module.exports = nextConfig
