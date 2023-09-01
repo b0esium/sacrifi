dd1c7bdb8e453dd20d56726d70aa364439f96844
b0esium <b0esium@protonmail.com>
2023-09-01 18:19:13 +0200

    Improve UI

--- app/components/NFTDisplay.js
+++ app/components/NFTDisplay.js
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import { useAccount } from "wagmi"
 import { Network, Alchemy } from "alchemy-sdk"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export default function NFTDisplay() {
     const { address } = useAccount()
@@ -12,16 +12,23 @@ export default function NFTDisplay() {
     const alchemy = new Alchemy(settings)
 
     const [nfts, setNfts] = useState([])
+    // load NFT data when this component is mounted
+    useEffect(() => {
+        getNfts()
+    }, [])
 
     async function getNfts() {
+        // exclude spam NFTs known by the Alchemy API
         const excludeFilters = "SPAM"
         const nftsForOwner = await alchemy.nft.getNftsForOwner(address, excludeFilters)
 
         console.log("number of NFTs found:", nftsForOwner.totalCount)
 
+        // exclude NFTs with no title (usually spam)
         let filteredNfts = nftsForOwner.ownedNfts.filter((nft) => {
             return nft.title !== ""
         })
+        // exclude NFTs with no images cached by Alchemy API (usually spam)
         filteredNfts = filteredNfts.filter((nft) => {
             return nft.media[0].gateway.includes("nft-cdn.alchemy.com")
         })
@@ -32,34 +39,16 @@ export default function NFTDisplay() {
     }
 
     return (
-        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-            <button className="rounded" onClick={getNfts}>
-                Get NFTs
-            </button>
-
-            <ul>
-                {nfts.map((nft, index) => (
-                    <div key={index} className="group">
-                        <li>{nft.title}</li>
-                        <Image
-                            src={nft.media[0].gateway}
-                            width={100}
-                            height={100}
-                            alt={nft.title}
-                        />
-                    </div>
-                ))}
-            </ul>
-
-            {/* <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
-                <h2 className={`mb-3 text-2xl font-semibold`}>
-                    NFT 1{" "}
-                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                        -&gt;
-                    </span>
-                </h2>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>{address}</p>
-            </div>             */}
+        <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-center">
+            {nfts.map((nft, index) => (
+                <div
+                    key={index}
+                    className="group justify-self-start rounded-lg border px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+                >
+                    <h4 className={`mb-3 text-l font-semibold`}>{nft.title}</h4>
+                    <Image src={nft.media[0].gateway} width={200} height={200} alt={nft.title} />
+                </div>
+            ))}
         </div>
     )
 }
