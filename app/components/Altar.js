353a87ce08ddd60bc5bd11fc935edaf6b8f9d350
b0esium <b0esium@protonmail.com>
2023-09-01 16:01:37 +0200

    Get NFT list from user's account

--- app/components/Altar.js
+++ app/components/Altar.js
@@ -1,12 +1,15 @@
 export default function Altar() {
+    function msg() {
+        alert("bloup")
+    }
     return (
-        <div>
+        <div className="grid">
             <div className="flex items-center justify-center p-8 aspect-square rounded-lg border-neutral-700 bg-neutral-800 bg-opacity-30">
                 NFT to be sacrificed
             </div>
-            <div className="flex justify-center">
-                <button className="p-4 bg-red-500 rounded">Sacrify</button>
-            </div>
+            <button onClick={() => msg()} className="p-4 bg-red-500 rounded">
+                Sacrify
+            </button>
         </div>
     )
 }
--- app/components/NFTDisplay.js
+++ app/components/NFTDisplay.js
@@ -1,60 +1,54 @@
+import Image from "next/image"
+import { useAccount } from "wagmi"
+import { Network, Alchemy } from "alchemy-sdk"
+import { useState } from "react"
+
 export default function NFTDisplay() {
+    const { address } = useAccount()
+    const settings = {
+        apiKey: process.env.ALCHEMY_API_KEY,
+        network: Network.ETH_MAINNET,
+    }
+    const alchemy = new Alchemy(settings)
+
+    const [nfts, setNfts] = useState([])
+    const [titles, setTitles] = useState([])
+
+    async function getNfts() {
+        const nftsForOwner = await alchemy.nft.getNftsForOwner(address)
+        console.log("number of NFTs found:", nftsForOwner.totalCount)
+        setNfts([...nftsForOwner.ownedNfts])
+        const titles = nfts.map((nft) => nft.title)
+        setTitles([...titles])
+        // for (const nft of nftsForOwner.ownedNfts) {
+        //     console.log(nft.title)
+        //     // if (nft.media[0].gateway) {
+        //     //     console.log(nft.media[0].gateway)
+        //     // }
+        // }
+    }
+
     return (
         <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-            <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
+            <button className="rounded" onClick={getNfts}>
+                Get NFTs
+            </button>
+
+            <ul>
+                {titles.map((title, index) => (
+                    <li key={index}>{title}</li>
+                ))}
+            </ul>
+
+            {/* <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
                 <h2 className={`mb-3 text-2xl font-semibold`}>
                     NFT 1{" "}
                     <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
                         -&gt;
                     </span>
                 </h2>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>NFT information</p>
-            </div>
-
-            <a
-                href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <h2 className={`mb-3 text-2xl font-semibold`}>
-                    NFT 2{" "}
-                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                        -&gt;
-                    </span>
-                </h2>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>NFT information</p>
-            </a>
-
-            <a
-                href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <h2 className={`mb-3 text-2xl font-semibold`}>
-                    NFT 3{" "}
-                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                        -&gt;
-                    </span>
-                </h2>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>NFT information</p>
-            </a>
-
-            <a
-                href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-                className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-                <h2 className={`mb-3 text-2xl font-semibold`}>
-                    NFT 4{" "}
-                    <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-                        -&gt;
-                    </span>
-                </h2>
-                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>NFT information</p>
-            </a>
+                <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>{address}</p>
+            </div>             */}
         </div>
     )
 }
